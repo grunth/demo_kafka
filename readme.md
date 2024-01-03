83e0ccbf867e50fb8b690acc0ce78cd47ae51f61
grigory osipov <vn56@bk.ru>
2024-01-04 00:11:58 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+kafka-topics --create --bootstrap-server localhost:29092 --replication-factor 1 --partitions 1 --topic mytopic
---
+++ src/main/java/com/example/demo_kafka/DemoKafkaApplication.java
@@ -0,0 +1,39 @@
+package com.example.demo_kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class DemoKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKafkaApplication.class, args);
+	}
+
+	@Bean
+	public NewTopic topic() {
+		return TopicBuilder.name("topic1")
+				.partitions(10)
+				.replicas(1)
+				.build();
+	}
+
+	@KafkaListener(id = "myId", topics = "topic1")
+	public void listen(String in) {
+		System.out.println(in);
+	}
+
+	@Bean
+	public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+		return args -> {
+			template.send("topic1", "test");
+		};
+	}
+
+}
